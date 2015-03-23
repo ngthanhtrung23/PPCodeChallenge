095c0ec50e6b53aec3b1fce73b10f884ea0a5a02
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-23 17:03:03 +0800

    Add solution + test for Pet detective

---
+++ PetDetective/sol1.cpp
@@ -0,0 +1,89 @@
+// Author: Do Ngoc Khanh
+#include <stdio.h>
+#include <string.h>
+#include <deque>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef pair<ii, ii> quad;
+#define X first
+#define Y second
+
+quad rect(int x, int y, int u, int v)
+    { return quad(ii(x, y), ii(u, v)); }
+
+const int N = 40;
+int m, n, k, b[123][123], bx[123][123], by[123][123], id[123][123], MaxID=0;
+int a[N][4], Special[N]; //
+short d[N][1<<11][1<<11];
+
+bool minimize(short &a, int b)
+    { return a>b ? a=b, true : false; }
+
+int bfs(int u) {
+    deque<quad> qu;
+    memset(d, 0x3c, sizeof d);
+    qu.push_back(rect(0, u, 0, 0));
+
+    while (qu.size()) {
+        quad Pop=qu.front(); qu.pop_front();
+        int du=Pop.X.X, u=Pop.X.Y, u1=Pop.Y.X, u2=Pop.Y.Y;
+        if (!minimize(d[u][u1][u2], du)) continue;
+        if (u2==(1<<k)-1) return du;
+        //printf("d[%d][%d][%d]=%d\n", u, u1, u2, du);
+
+        if (int x=Special[u]) {
+            if (x>0 && ((1<<x-1)&(~u1)) && ((1<<x-1)&~u2) && __builtin_popcount(u1)<4)
+            qu.push_front(rect(du, u, u1|(1<<x-1), u2));
+            if (x<0 && ((1<<-x-1)&(u1)) && ((1<<-x-1)&~u2))
+            qu.push_front(rect(du, u, u1^(1<<-x-1), u2|(1<<-x-1)));
+        }
+        for (int i=0; i<4; i++) if (int v=a[u][i])
+            qu.push_back(rect(du+1, v, u1, u2));
+    }
+    return -1;
+}
+
+main() {
+    int sx, sy;
+    scanf("%d%d", &m, &n);
+    for (int i=1; i<=m; i++)
+    for (int j=1; j<=n; j++) {
+        scanf("%d", &b[i][j]);
+        if (b[i][j]==99)
+            { sx=i, sy=j; b[i][j]=0; }
+        else if (b[i][j]>0)
+            k++;
+        id[i][j] = ++MaxID;
+        Special[id[i][j]]=b[i][j];
+    } // done k, id, sx, sy
+
+    for (int i=1; i<=m; i++)
+    for (int j=1; j<n; j++) {
+        scanf("%d", &bx[i][j]);
+        #define connect(x, y, k) a[x][k]=y
+        if (bx[i][j]) {
+            connect(id[i][j], id[i][j+1], 0);
+            connect(id[i][j+1], id[i][j], 1);
+        }
+    }
+    for (int i=1; i<m; i++)
+    for (int j=1; j<=n; j++) {
+        scanf("%d", &by[i][j]);
+        if (by[i][j]) {
+            connect(id[i][j], id[i+1][j], 2);
+            connect(id[i+1][j], id[i][j], 3);
+        }
+    }
+    /*
+    for (int i=1; i<=m; i++)
+    for (int j=1; j<=n; j++)
+    printf(j==n ? "%d <--- id\n" : "%d ", id[i][j]);
+
+    for (int i=1; i<=MaxID; i++)
+    printf("%d %d %d %d <-- %d(%d)\n", a[i][0], a[i][1], a[i][2], a[i][3], i, Special[i]);
+    */
+    cout << bfs(id[sx][sy]) << endl;
+}
---
+++ PetDetective/sol2.cpp
@@ -0,0 +1,141 @@
+// Author: Nguyen Thanh Trung
+#include <bits/stdc++.h>
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define EACH(it,a) for(__typeof(a.begin()) it = a.begin(); it != a.end(); ++it)
+
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+#define sqr(x) ((x) * (x))
+using namespace std;
+
+int m, n, a[11][11], ngang[11][11], doc[11][11];
+pair<int,int> from[111], to[111];
+int p3[111];
+int d[111][111];
+
+int getBit(int mask, int u) {
+    return (mask % p3[u+1]) / p3[u];
+}
+int setBit(int mask, int u, int val) {
+    return mask + p3[u] * (val - getBit(mask, u));
+}
+int getId(int u, int v) {
+    return (u-1) * n + v;
+}
+
+void addEdge(int i1, int j1, int i2, int j2) {
+    int u = getId(i1, j1), v = getId(i2, j2);
+    d[u][v] = d[v][u] = 1;
+}
+
+struct State {
+    int mask;
+    int u, v;
+    int cap;
+
+    State() {}
+    State(int mask, int u, int v, int cap) : mask(mask), u(u), v(v), cap(cap) {}
+};
+
+bool operator < (const State& a, const State& b) {
+    if (a.mask != b.mask) return a.mask < b.mask;
+    if (a.u != b.u) return a.u < b.u;
+    if (a.v != b.v) return a.v < b.v;
+    if (a.cap != b.cap) return a.cap < b.cap;
+    return false;
+}
+
+map<State,State> traceState;
+map<State,int> traceMove;
+
+void trace(State cur) {
+    if (cur.mask == 0) return ;
+    trace(traceState[cur]);
+    cout << traceMove[cur] << endl;
+}
+
+int main() {
+    p3[0] = 1; FOR(i,1,20) p3[i] = p3[i-1] * 3;
+    while (cin >> m >> n) {
+        int starti, startj, bound = 0;
+        FOR(i,1,m) FOR(j,1,n) {
+            cin >> a[i][j];
+            if (a[i][j] == 99) {
+                starti = i;
+                startj = j;
+            }
+            else if (a[i][j] > 0) from[a[i][j]] = make_pair(i, j), bound = max(bound, a[i][j]);
+            else if (a[i][j] < 0) to[-a[i][j]] = make_pair(i, j), bound = max(bound, -a[i][j]);
+        }
+        FOR(i,1,m) FOR(j,1,n-1) cin >> ngang[i][j];
+        FOR(i,1,m-1) FOR(j,1,n) cin >> doc[i][j];
+
+        cerr << "DONE INPUT" << endl;
+
+        FOR(i,1,100) FOR(j,1,100) d[i][j] = (i == j) ? 0 : 1000111000;
+        FOR(i,1,m) FOR(j,1,n) {
+            if (ngang[i][j]) addEdge(i, j, i, j+1);
+            if (ngang[i][j-1]) addEdge(i, j, i, j-1);
+            if (doc[i][j]) addEdge(i, j, i+1, j);
+            if (doc[i-1][j]) addEdge(i, j, i-1, j);
+        }
+        FOR(k,1,100) FOR(i,1,100) FOR(j,1,100) d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+
+//        FOR(i,1,m*n) {
+//            FOR(j,1,m*n) cout << d[i][j] << ' ';
+//            cout << endl;
+//        }
+
+        cerr << "DONE FLOYD" << endl;
+
+        map<State, int> dist;
+        set< pair<int, State> > all;
+        all.insert(make_pair(0, State(0, starti, startj, 4)));
+
+        while (!all.empty()) {
+            long long l = all.begin()->first;
+            State cur = all.begin()->second;
+            all.erase(all.begin());
+
+            if (cur.mask == p3[bound]-1) {
+                DEBUG(l);
+                trace(cur);
+                break;
+            }
+
+            if (l != dist[cur]) continue;
+
+            FOR(i,1,bound) {
+                int t = getBit(cur.mask, i-1);
+                if (t == 0 && cur.cap > 0) {
+                    State next(setBit(cur.mask, i-1, 1), from[i].first, from[i].second, cur.cap-1);
+                    long long l2 = l + d[getId(cur.u, cur.v)][getId(from[i].first, from[i].second)];
+
+                    if (!dist.count(next) || dist[next] > l2) {
+                        dist[next] = l2;
+                        all.insert(make_pair(l2, next));
+                        traceState[next] = cur;
+                        traceMove[next] = i;
+                    }
+                }
+                else if (t == 1) {
+                    State next(setBit(cur.mask, i-1, 2), to[i].first, to[i].second, cur.cap+1);
+                    long long l2 = l + d[getId(cur.u, cur.v)][getId(to[i].first, to[i].second)];
+
+                    if (!dist.count(next) || dist[next] > l2) {
+                        dist[next] = l2;
+                        all.insert(make_pair(l2, next));
+                        traceState[next] = cur;
+                        traceMove[next] = -i;
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
